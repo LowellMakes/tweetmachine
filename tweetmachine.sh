960ef5115739e080810c7778a7ec85a0e62c81c3
leftyfb <leftyfb@left-click.org>
2014-01-12 22:03:46 -0500

    initial add

---
+++ tweetmachine.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+log="/home/leftyfb/twitterlog.txt"
+ignore="/home/leftyfb/twitterignorelist.txt"
+gennumber=$(seq 1000 9999 | sort -R | head -1)
+
+gettweet(){
+	pulltweet=$(/home/leftyfb/ttytter -runcommand="/replies" |tail -n1)
+	tweet=$pulltweet
+	name=$(echo $tweet|awk -F '[<|>]' '{print $2}')
+	tnumber=$(echo $tweet|grep -Eo '[0-9]{4}')
+	alreadytweeted=$(grep ^$name$ $ignore|wc -l)
+	sleep 30
+}
+
+if [ -n "$1" ] ;then
+	number=$1
+else
+	number=$gennumber
+fi
+echo $number
+
+COUNTER=0
+
+while [ $COUNTER -lt "4" ]; do
+		gettweet
+		echo "tweet = $tweet"
+		echo "name = $name"
+		echo "number = $number"
+		echo "tnumber = $tnumber"
+		echo "alreadytweeted = $alreadytweeted"
+		if [ -z "$tweet" ] ;then
+			echo "Something is wrong"
+			exit 1
+		fi
+		if [ "$number" = "$tnumber" ];then
+			if [ $alreadytweeted = "1" ]; then
+				/home/leftyfb/ttytter -runcommand="@$name Sorry, but you already got a prize."
+				echo "tweeted back, $name already tweeted"
+				exit 0
+			elif [ $alreadytweeted = "0" ]; then
+				echo "@$name tweeted the correct number $tnumber"
+				echo "dispense toy"
+				echo "$(date) $name" >> $log
+				echo "$name" >> $ignore
+				exit 0
+			fi
+		fi
+		let COUNTER=COUNTER+1
+		echo "COUNTER = $COUNTER"
+done
