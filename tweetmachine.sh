34123cd52aa42bf9b32444dc915b7608266a8096
leftyfb <leftyfb@left-click.org>
2014-01-12 22:42:26 -0500

    fixed ttytter binary path

--- tweetmachine.sh
+++ tweetmachine.sh
@@ -3,25 +3,28 @@
 log="twitterlog.txt"
 ignore="twitterignorelist.txt"
 gennumber=$(seq 1000 9999 | sort -R | head -1)
-ttytter=$(/usr/bin/ttytter)
-
-gettweet(){
-	pulltweet=$($ttytter -runcommand="/replies" |tail -n1)
-	tweet=$pulltweet
-	name=$(echo $tweet|awk -F '[<|>]' '{print $2}')
-	tnumber=$(echo $tweet|grep -Eo '[0-9]{4}')
-	alreadytweeted=$(grep ^$name$ $ignore|wc -l)
-	sleep 30
-}
+ttytter="/home/leftyfb/ttytter"
 
 if [ -n "$1" ] ;then
 	number=$1
 else
 	number=$gennumber
 fi
-echo $number
+#echo "gennumber = $gennumber"
+echo "number = $number"
 
+echo "setting COUNTER = 0"
 COUNTER=0
+
+gettweet(){
+	pulltweet=$($ttytter -runcommand="/replies" |tail -n1)
+	echo $($ttytter -runcommand="/replies")
+	tweet=$pulltweet
+	name=$(echo $tweet|awk -F '[<|>]' '{print $2}')
+	tnumber=$(echo $tweet|grep -Eo '[0-9]{4}')
+	alreadytweeted=$(grep ^$name$ $ignore|wc -l)
+	sleep 30
+}
 
 while [ $COUNTER -lt "4" ]; do
 		gettweet
@@ -41,6 +44,7 @@ while [ $COUNTER -lt "4" ]; do
 				exit 0
 			elif [ $alreadytweeted = "0" ]; then
 				echo "@$name tweeted the correct number $tnumber"
+				$ttytter -runcommand="Congrats to @$name for his free tweetmachine prize!"
 				echo "dispense toy"
 				echo "$(date) $name" >> $log
 				echo "$name" >> $ignore
